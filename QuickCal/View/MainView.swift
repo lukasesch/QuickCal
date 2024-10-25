d75b6b93775d77fcc5df777db2472743ac95d570
lukasesch <lukasesch@gmail.com>
2024-10-25 16:10:23 +0200

    Mockup Circles for kcal and macro progress

--- QuickCal/View/MainView.swift
+++ QuickCal/View/MainView.swift
@@ -13,6 +13,10 @@ struct MainView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @AppStorage("onboarding") private var onboardingDone = false
     @State private var currentPage = 1
+    @State private var kcalProgressPercentage = 0.45
+    @State private var carbohydrateProgressPercentage = 0.18
+    @State private var proteinProgressPercentage = 0.37
+    @State private var fatProgressPercentage = 0.11
 
 
     
@@ -27,6 +31,32 @@ struct MainView: View {
                 
                 
                 VStack {
+                    Text("QuickCal")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                    Divider()
+                    Spacer()
+                    CircularProgressView(barColor: .blue, barWidth: 25, progressPercentage: kcalProgressPercentage)
+                        .frame(width: 120, height: 120)
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        CircularProgressView(barColor: .green, barWidth: 15, progressPercentage: carbohydrateProgressPercentage)
+                            .frame(width: 80, height: 80)
+                        Spacer()
+                        CircularProgressView(barColor: .orange, barWidth: 15, progressPercentage: proteinProgressPercentage)
+                            .frame(width: 80, height: 80)
+                        Spacer()
+                        CircularProgressView(barColor: .purple, barWidth: 15, progressPercentage: fatProgressPercentage)
+                            .frame(width: 80, height: 80)
+                        Spacer()
+                    }
+                    Spacer()
+                    Divider()
+                    Spacer()
+                    Spacer()
+                    
                     Text("Dein täglicher Kalorienbedarf beträgt")
                     Text("\(mainViewModel.dailyCalories, specifier: "%.0f") kcal")
                         .font(.title2)
@@ -35,12 +65,13 @@ struct MainView: View {
                     Button(action: {
                         onboardingDone = false
                     }) {
-                        Text("Reset")
+                        Text("Reset Profile")
                             .padding()
                             .background(Color.red)
                             .foregroundColor(.white)
                             .cornerRadius(8)
                     }
+                    Spacer()
                 }
                 .navigationBarBackButtonHidden(true)
                 .onAppear {
@@ -53,6 +84,7 @@ struct MainView: View {
                 .tag(1)
                 
                 
+                
                 // Add Item View
                 AddItemView()
                     .tabItem { Text("Add Item") } // TabItem für AddItemView
@@ -63,6 +95,34 @@ struct MainView: View {
         }
     }
     
+    struct CircularProgressView: View {
+        var barColor: Color
+        var barWidth: CGFloat
+        var progressPercentage: CGFloat
+        var body: some View {
+            ZStack {
+                Circle()
+                    .stroke(
+                        barColor.opacity(0.4),
+                        lineWidth: barWidth
+                    )
+
+                Circle()
+                    .trim(from: 0, to: progressPercentage)
+                    .stroke(
+                        barColor,
+                        lineWidth: barWidth
+                    )
+                    .rotationEffect(Angle(degrees: -90))
+                Text("""
+                     \(progressPercentage, specifier: "%.2f")%
+                     """)
+                .fontWeight(barWidth > 20 ? .bold : .regular)
+                
+                .multilineTextAlignment(.center)
+            }
+        }
+    }
 }
 
 #Preview {
