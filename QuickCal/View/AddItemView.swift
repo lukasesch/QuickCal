6aebb40d0fa0fa2f39852e13faab25ce13903dce
lukasesch <lukasesch@gmail.com>
2024-10-31 20:04:42 +0100

    Add food view mockup added

--- QuickCal/View/AddItemView.swift
+++ QuickCal/View/AddItemView.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct AddItemView: View {
     var body: some View {
-        Text("New Items can be manually added here")
+        VStack {
+            HStack {
+                Text("QuickCal")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.leading)
+                    .padding(.top, 5.0)
+                    .padding(.bottom, -2.0)
+            }
+            .padding(.horizontal, 25.0)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            VStack {
+                Button(action: {}) {
+                    Text("Hinzufügen")
+                        .font(.title2)
+                        .frame(maxWidth: .infinity, maxHeight: 80)
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                HStack {
+                    Button(action: {}) {
+                        Text("Lebensmittel erstellen")
+                            .frame(maxWidth: .infinity, maxHeight: 40)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+                    Button(action: {}) {
+                        Text("Gericht erstellen")
+                            .frame(maxWidth: .infinity, maxHeight: 40)
+                            .fontWeight(.semibold)
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+                }
+                Text("Zuletzt hinzugefügt:")
+            }
+            .padding()
+            
+        }
+        Spacer()
     }
 }
 
--- QuickCal/View/BarCodeView.swift
+++ QuickCal/View/BarCodeView.swift
@@ -8,20 +8,10 @@
 import SwiftUI
 
 struct BarCodeView: View {
-    @AppStorage("onboarding") private var onboardingDone = false
     
     var body: some View {
         VStack {
             Text("Scanning product barcode will be added here")
-                                Button(action: {
-                                    onboardingDone = false
-                                }) {
-                                    Text("Reset Profile")
-                                        .padding()
-                                        .background(Color.red)
-                                        .foregroundColor(.white)
-                                        .cornerRadius(8)
-                                }
         }
         
         
